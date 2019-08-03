9784989277c85a165858224c5ff178e5b294c9b4
George Tsitsiridis <gt@out-there-media.com>
2019-08-04 02:03:48 +0300

    Load RDS objects instead of csv files.

--- global.R
+++ global.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(plyr)
 #library(Seurat)
 library(cowplot)
-library(readxl)
+# library(readxl)
 library(shiny)
 library(shinyjs)
 library(shinydashboard)
@@ -27,49 +27,65 @@ checkFile <- function(f) {
 css_file = "www/style.css"
 source("functions.R")
 
-adi_at1_path <- "data/ADI_AT1_trajectory.RData"
+adi_at1_path <- "data/adi_at1.RDS"
 checkFile(adi_at1_path)
-load(adi_at1_path)
-convergence_path = "data/Convergence_trajectory.RData"
+adi_at1 <- readRDS(adi_at1_path)
+
+adi_at1_annot_path <- "data/adi_at1_annot.RDS"
+checkFile(adi_at1_annot_path)
+adi_at1_annot <- readRDS(adi_at1_annot_path)
+
+convergence_path <- "data/adi_at1.RDS"
+checkFile(adi_at1_path)
+adi_at1 <- readRDS(adi_at1_path)
+
+convergence_path <- "data/convergence.RDS"
+checkFile(convergence_path)
+convergence <- readRDS(convergence_path)
+
+convergence_annot_path <- "data/convergence_annot.RDS"
+checkFile(convergence_annot_path)
+convergence_annot <- readRDS(convergence_annot_path)
+
+convergence_path = "data/v01/Convergence_trajectory.RData"
 checkFile(convergence_path)
 load(convergence_path)
 
 ## Whole Lung Files
 filename = "data/WholeLung_data.h5" 
 checkFile(filename)
-metafile_path = "data/WholeLung_metadata.txt"
+metafile_path = "data/wholelung_metafile.RDS"
 checkFile(metafile_path)
-metafile <- read.delim(metafile_path, sep = "\t", header = T, stringsAsFactors = F)
-genes_path <- "data/WholeLung_genes.txt"
+metafile <- readRDS(metafile_path)
+genes_path <- "data/wholelung_genes.RDS"
 checkFile(genes_path)
-genes <- read.delim(genes_path, stringsAsFactors = F)[, 1]
-markers_table_path <- "data/S1_Bleo_AllMarkers_annotated.xlsx"
+genes <- readRDS(genes_path)
+markers_table_path <- "data/wholelung_markers_table.RDS"
 checkFile(markers_table_path)
-markers_table <- data.frame(read_excel(markers_table_path))
+markers_table <- readRDS(markers_table_path)
 
 ## High Resolution Epithel Files
 epi_filename = "data/Epi_highres_data.h5"
 checkFile(epi_filename)
-epi_metafile_path <- "data/HighresEpi_meta.txt"
+epi_metafile_path <- "data/epi_metafile.RDS"
 checkFile(epi_metafile_path)
-epi_metafile <- read.delim(epi_metafile_path, sep = "\t", header = T, stringsAsFactors = F)
-epi_genes_path <- "data/HighresEpi_genes.txt"
+epi_metafile <- readRDS(epi_metafile_path)
+epi_genes_path <- "data/epi_genes.RDS"
 checkFile(epi_genes_path)
-epi_genes <- read.delim(epi_genes_path, stringsAsFactors = F)[, 1]
-epi_markers_table_path <- "data/AllMarkers_Epi_inOne.xlsx"
+epi_genes <- readRDS(epi_genes_path)
+epi_markers_table_path <- "data/epi_markers_table.RDS"
 checkFile(epi_markers_table_path)
-epi_markers_table <- data.frame(read_excel(epi_markers_table_path,
-                                           col_types = c("text", "numeric", "numeric", "numeric", "text", "text")))
-spline_expr_path <- "data/Table_S2_adjpval_025.txt"
+epi_markers_table <- readRDS(epi_markers_table_path)
+spline_expr_path <- "data/spline_expr.RDS"
 checkFile(spline_expr_path)
-spline_expr <- read.delim(spline_expr_path, stringsAsFactors = F)
+spline_expr <- readRDS(spline_expr_path)
 
 ## Table from Spline fits for Cell-Cell Communication
-rec_lig_path <- "data/RecLig_merged_withSpline_adapted.txt"
+rec_lig_path <- "data/rec_lig.RDS"
 checkFile(rec_lig_path)
-rec_lig <- read.delim(file = rec_lig_path, sep = "\t", stringsAsFactors = F)
+rec_lig <- readRDS(rec_lig_path)
 
 ## Whole Lung Spline Table
-wholeLung_spline_path <- "data/WholeLung_spline.RData"
+wholeLung_spline_path <- "data/wholelung_spline.RDS"
 checkFile(wholeLung_spline_path)
-load(wholeLung_spline_path)
+wholeLung_spline <- readRDS(wholeLung_spline_path)
